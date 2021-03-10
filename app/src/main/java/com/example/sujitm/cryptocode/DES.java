a5e0d977be5781138e7b8a6755aa81791282291a
smesh <71513291+sujitmeshram@users.noreply.github.com>
2021-03-10 19:36:22 +0530

    Update DES.java

--- app/src/main/java/com/example/sujitm/cryptocode/DES.java
+++ app/src/main/java/com/example/sujitm/cryptocode/DES.java
@@ -110,11 +110,11 @@ public class DES extends AppCompatActivity {
 
                 String temp=input_text.getText().toString();
                 ans=encrypt(temp);
-                Log.d("NIKHIL", "encrypt key:" + ans);
+                Log.d("SUJIT", "encrypt key:" + ans);
                 output_text.setText(ans);
                /* String sans=ans;
                 sans=decrypt(sans);
-                Log.d("NIKHIL","decrypt key: "+sans);*/
+                Log.d("SUJIT","decrypt key: "+sans);*/
             }
         });
 
@@ -123,7 +123,7 @@ public class DES extends AppCompatActivity {
             public void onClick(View view) {
                 String temp=input_text.getText().toString();
                 ans=decrypt(temp);
-                Log.d("NIKHIL", "decrypt key:" + ans);
+                Log.d("SUJIT", "decrypt key:" + ans);
                 output_text.setText(ans);
             }
         });
